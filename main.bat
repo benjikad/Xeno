6a44d86a39e5b517a9baedfc56f8e6be712c662d
benjikad <123773963+benjikad@users.noreply.github.com>
2025-08-18 19:32:46 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,8 +1,10 @@
 @echo off
+title Immediate action required!
 
+echo.
 echo You use encountered a Xeno vulnerability.
 echo Continuing to use this executor puts you at serious risk.
-echo.
+echo Only run scripts that you know are safe.
 echo.
 
 pause
